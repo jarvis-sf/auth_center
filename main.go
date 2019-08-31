b5a3d537292a20e9d072d3c20cadcb0e53d996c5
zhangyun <zhangyun@eyxyt.com>
2019-08-31 15:31:14 +0800

    init project

--- main.go
+++ main.go
@@ -6,12 +6,10 @@ import (
 )
 
 func main() {
-
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.EnableDocs = true
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
-
 	beego.Run()
 }
